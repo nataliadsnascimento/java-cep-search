247209f01b4dc7cbb8db9ef4d08772d6e6fc5a6c
Natália Nascimento <nataliasanntos161@gmail.com>
2026-05-29 16:00:51 -0300

    fix: adiciona validação para CEP inválido

--- src/main/java/org/buscadorCep/ConsultaCep.java
+++ src/main/java/org/buscadorCep/ConsultaCep.java
@@ -8,22 +8,31 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaCep {
+
     public Endereco buscarEndereco(String cep) {
+
         URI endereco = URI.create("https://viacep.com.br/ws/"+ cep+"/json");
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = null;
+        HttpResponse<String> response;
+
         try {
-            response = HttpClient
-                    .newHttpClient()
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+            response = client.send(request, HttpResponse.BodyHandlers.ofString()
+            );
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Não é possível obter o endereço a partir deste CEP");
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
+        String json = response.body();
+
+        if (json.contains("\"erro\": true")) {
+
+            throw new RuntimeException("CEP não encontrado.");
+        }
+
+        return new Gson().fromJson(json, Endereco.class);
     }
 }
